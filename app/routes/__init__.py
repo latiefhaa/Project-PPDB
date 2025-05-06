9c3d2527ff54c938cc5746cd8de8fd66f24a879d
latiefhaa <ilhameskepal@gmail.com>
2025-05-06 09:08:00 +0700

    add route initialization with blueprints for auth, main, and admin

---
+++ app/routes/__init__.py
@@ -0,0 +1,8 @@
+from .auth import auth_bp
+from .main import main_bp
+from .admin import admin_bp
+
+def register_blueprints(app):
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(main_bp)
+    app.register_blueprint(admin_bp)
